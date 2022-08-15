7d48947bfcf10e66135ecfd53369c33f092a925f
LeraPazgert <pazgertjob@gmail.com>
2022-08-15 20:35:44 +0300

    added types

---
+++ src/models/models.ts
@@ -0,0 +1,12 @@
+export interface ICoin {
+  data:[];
+  id: string;
+  rank: string;
+  symbol: string;
+  name: string;
+  supply: string;
+  marketCapUsd: string;
+  priceUsd: string;
+  vwap24Hr: string;
+}
+
