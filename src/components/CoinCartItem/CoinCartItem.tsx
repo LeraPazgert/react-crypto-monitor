635f336380e915c9dcb9920ac7a484e3aa0cd9c9
LeraPazgert <pazgertjob@gmail.com>
2022-09-27 15:08:42 +0300

    added local state and tooltip

--- src/components/CoinCartItem/CoinCartItem.tsx
+++ src/components/CoinCartItem/CoinCartItem.tsx
@@ -1,30 +1,48 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useAppDispatch } from '../../hooks/redux';
 import { ICoinCart } from '../../models/models';
 import { coinDeleted } from '../../store/slices/coinCartSlice';
+import { Tooltip } from "@mui/material";
 
 interface CoinCartItemProps {
     coin: ICoinCart
 }
 const CoinCartItem = ({ coin }: CoinCartItemProps) => {
+    const [active, setActive] = useState(false);
     const dispatch = useAppDispatch();
 
     const handleDelete = useCallback((): void => {
         dispatch(coinDeleted(coin.id));
     }, [coin.id, dispatch]);
 
+    const handleShowFullAmount = () => {
+        setActive(!active);
+    };
+
     return (
         <div>
-            <div className="briefcase__wrapper">
+            <div className="briefcase__main-wrapper">
+
                 <div className="briefcase__coin">
-                    {coin.symbol} {coin.quantity}
+                    {coin.symbol} {active ? coin.quantity : coin.quantity.toFixed(3)}
                 </div>
-                <button
-                    onClick={handleDelete}
-                    className="briefcase__btn"
-                >
-                    Delete
-                </button>
+                <div className="briefcase__wrapper">
+                    <Tooltip title='Show full amount' arrow>
+                        <button className="briefcase__button-show" onClick={handleShowFullAmount}>
+                            <img src="https://img.icons8.com/ios-glyphs/60/000000/visible--v1.png" alt='full amount' />
+                        </button>
+                    </Tooltip>
+                    <Tooltip title='Remove from wallet' arrow>
+                        <button
+                            onClick={handleDelete}
+                            className="briefcase__button-delete"
+                        >
+                            Delete
+                        </button>
+                    </Tooltip>
+
+                </div>
+
             </div>
         </div>
     );
