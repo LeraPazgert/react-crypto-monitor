e179ab16e0538020ce0a8eb40096efa753980f8c
LeraPazgert <pazgertjob@gmail.com>
2022-09-27 15:10:09 +0300

    added close-button

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,13 +3,14 @@ import "./Modal.scss";
 
 interface ModalProps {
     active: boolean
-    setActive:(active: boolean)=>void
+    setActive: (active: boolean) => void
     children: React.ReactNode,
 }
 
 const Modal = ({ active, setActive, children }: ModalProps) => {
     return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+        <div className={active ? "modal active" : "modal"}>
+            <div onClick={() => setActive(false)} className='close-button'>X</div>
             <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
                 {children}
             </div>
